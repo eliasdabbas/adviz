601193f57a15585b05a9d0716164090f10b6b027
Elias Dabbas <eliasdabbas@gmail.com>
2023-04-24 13:12:17 +0300

    Add new racing_chart function

--- adviz/__init__.py
+++ adviz/__init__.py
@@ -3,3 +3,4 @@ __version__ = "0.0.10"
 from .value_counts_plus import value_counts_plus
 from .status_codes import status_codes
 from .url_structure import url_structure
+from .racing_chart import racing_chart

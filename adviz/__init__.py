778cffad494409c378d84935eafd6135995dc4ad
Elias Dabbas <eliasdabbas@gmail.com>
2023-07-03 08:42:31 +0200

    Add flag function functionality

--- adviz/__init__.py
+++ adviz/__init__.py
@@ -4,3 +4,4 @@ from .value_counts_plus import value_counts_plus
 from .status_codes import status_codes
 from .url_structure import url_structure
 from .racing_chart import racing_chart
+from .flag import flag, country_code_flag

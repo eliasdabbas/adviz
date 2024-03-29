e8d93cd12caeb53771b8172f44cefba63f729a5f
Elias Dabbas <eliasdabbas@gmail.com>
2024-03-29 12:49:21 +0300

    Fix import issue in documentation

--- adviz/racing_chart.py
+++ adviz/racing_chart.py
@@ -8,6 +8,7 @@ import pandas as pd
 import plotly.express as px
 from plotly.subplots import make_subplots
 import adviz
+from IPython.display import display_html
 
 # %% ../nbs/03_racing_chart.ipynb 4
 def racing_chart(

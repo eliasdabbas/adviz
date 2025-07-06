2e8e3da4ceaf09da7a855446c0827f10cd9f5168
Elias Dabbas <eliasdabbas@gmail.com>
2025-07-06 13:00:07 +0100

    Load all figure templates in init file on import

--- adviz/__init__.py
+++ adviz/__init__.py
@@ -1,11 +1,15 @@
 __version__ = "0.0.22"
 
-from .value_counts_plus import value_counts_plus
-from .value_counts import value_counts
-from .status_codes import status_codes
-from .url_structure import url_structure
-from .racing_chart import racing_chart
-from .flag import flag, country_code_flag
-from .style_table import style_table
-from .serp_heatmap import serp_heatmap
+from dash_bootstrap_templates import load_figure_template
+
 from .ecdf import ecdf
+from .flag import country_code_flag, flag
+from .racing_chart import racing_chart
+from .serp_heatmap import serp_heatmap
+from .status_codes import status_codes
+from .style_table import style_table
+from .url_structure import url_structure
+from .value_counts import value_counts
+from .value_counts_plus import value_counts_plus
+
+load_figure_template("all")
